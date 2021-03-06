c993d6af2cbb8c830f13c5b86126832ffe0fb4e6
Ulric Wilfred <shaman.sir@gmail.com>
2021-03-06 12:16:20 +0100

    the draft with parsing a file (failing for now)

--- fsq-swarm-to-kml.hs
+++ fsq-swarm-to-kml.hs
@@ -13,7 +13,7 @@
 import Data.Aeson
 import GHC.Generics
 import Data.ByteString.Lazy
-
+import qualified Data.ByteString.Lazy as B
 
 
 data Venue =
@@ -40,7 +40,11 @@ decodeCheckins = eitherDecode "{ \"venue\": { \"lat\": 10, \"long\": 20 } }"
 
 
 main :: IO ()
-main =
+main = do
+  input <- B.readFile "./20210301_0_50.json"
+  Prelude.putStrLn
+    $ show
+    $ (eitherDecode input :: Either String Checkin)
   Prelude.putStrLn
     $ show
     $ decodeCheckins
